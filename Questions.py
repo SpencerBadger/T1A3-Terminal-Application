6cdbb2c5820eaaad5915f890ea746c0781a10602
SpencerBadger <spbadger@hotmail.com>
2024-05-06 10:54:48 +1000

    Removed files - functions.py

---
+++ Questions.py
@@ -0,0 +1,77 @@
+import rich
+from rich.console import Console
+from rich.text import Text
+from rich.table import Table
+from rich import print
+
+console = Console()
+table = Table()
+data_table = [[],[],[],[]]
+
+def question_quiz():
+        questions = ("1. What is the name of the human capital city in World of Warcraft?",
+                "2. Which of these classes are available to blood elves in World of Warcraft?",
+                "3. Which four-wing instance located in Tirisfal Glades is frequently farmed by players in World of Warcraft?",
+                "4. Who built Karazhan in World of Warcraft?",
+                "5. What is the draenei racial heal called in World of Warcraft?" )
+
+        multiple_choice = (("A. Ironforge", "B. Elwynn City", "C. Stormwind City", "D. Silvermoon City", "E. Booty Bay"),
+                ("A. Warrior", "B. Warlock", "C. Priest", "D. Hunter","E. Rogue","F. All of the above"),
+                ("A. Scarlet Monastery", "B. Auchindoun", "C. The Stockade", "D. Shadowfang Keep","E. The Deadmines"),
+                ("A. Archimonde","B. Prince Kael'thas Sunstrider", "C. Guardian Aegwynn", "D. Kel'thuzad","E. Arthas Menethil"),
+                ("A. Blessing of Argus", "B. Gift of the Naaru", "C. Symbol of Hope", "D. Velen's Touch","E. Call of the Exodar"))
+        
+        answers = ("C","F","A","C","B")
+        long_answer =("C. Stormwind City","F. All of the above","A. Scarlet Monastery","C. Guardian Aegwynn","B. Gift of the Naaru")
+        guesses = []
+        score = []
+        question_number = 0
+
+        for question in questions:
+            console.print("[white] --------------------------------------------------------------------------------------------------------------------------------")
+            console.print("[green]" + question)
+            console.print("[white]---------------------------------------------------------------------------------------------------------------------------------")
+
+            for answer in multiple_choice[question_number]:
+                print(answer)
+            guesses = input("Please enter the corresponding letter: ").upper()
+            data_table[0].append(questions[question_number])
+            data_table[1].append(long_answer[question_number])
+            if guesses == answers[question_number]:
+                score
+                
+                console.print(":white_check_mark:" + "[green] Correct!", justify="center")
+                data_table[2].append("Correct")
+                data_table[3].append(score)
+            else:
+                console.print(":cross_mark:" + "[red] Incorrect!", justify="center")
+                data_table[2].append("Incorrect")
+            
+            console.print("[white]---------------------------------------------------------------------------------------------------------------------------------")
+            
+            question_number += 1
+
+        table = Table(title = "Results",show_lines=True)
+        table.add_column("Question", justify="left")
+        table.add_column("Answer", justify="left")
+        table.add_column("Correct/Incorrect", justify="left")
+        table.add_column("Score", justify="left")
+        for i in range(len(data_table[0])):
+            if data_table[2][i] == "Correct":
+                table.add_row(data_table[0][i],data_table[1][i],"[green] Correct",":white_check_mark:")
+            else:
+                 table.add_row(data_table[0][i],data_table[1][i],"[red] Incorrect",":cross_mark:")
+        # table.add_row("Total Score: ","","","1")
+        console.print(table)
+        print(type(data_table))
+        print(type(data_table[0]))
+        print(type(data_table[1]))
+        print(type(data_table[2]))
+        print(type(data_table[3]))
+#    SAVED USERS DETAILS AND SCORE
+
+        exit()
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,62 @@
-from main_quiz import main_quiz
-from main_quiz import quiz_header
 from colorama import Fore
-from pyfiglet import figlet_format
+import pyfiglet
+import csv
+from rich.console import Console
+from rich.table import Table
+from Questions import question_quiz
 
-#  Print list of choices from text file. Iterate over for i in i blah blah
+# def scores():
+#     try:
+#         quiz_questions = []
+#         scores = 'scores.json'
+#         with open(scores) as f:
+#             json.dump(quiz_questions, f)
+#     except:
+#         print("This didn't work")
 
-main_quiz()
+def quiz_header():
+    from pyfiglet import figlet_format
+    from colorama import Fore
+
+    result = pyfiglet.figlet_format("Quiz",font= 'slant', justify="center")
+    return (print(result))
+
+def main_quiz():
+    try:
+        quiz_header()
+        table = Table()
+        table.add_column("Option (1)", justify="center")
+        table.add_column("Option (2)", justify="center")
+        table.add_column("Option (3)", justify="center")
+        table.add_row(" Play Quiz","View Highscores"," Exit Game")
+        console = Console()
+        console.print(table)
+
+        choice = int(input("Please input your choice: "))
+        if choice == 1:
+            print("You have chosen to play the quiz!")
+            question_quiz()
+            
+           
+        elif choice == 2:
+            print("You have chosen to view highscores!")
+            # def view_highscores():
+            #         try:
+            #             with open('scores.csv') as r:
+            #                 for line in r:
+            #                     print(line)
+            #         except:
+            #             print("This didn't work")
+            # view_highscores()
+            main_quiz()
+
+        elif choice == 3:
+            print("You have chosen to exit the game!")
+            exit()
+        else:
+            main_quiz()
+    except ValueError:
+        console.print("\n You have entered an invalid choice! \n", justify="center")
+        console.print(":cross_mark:" + "[red] Please Enter a valid choice." + ":cross_mark: \n ", justify="center")
+        return(main_quiz())
+
